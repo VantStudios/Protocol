51be4a2031f59cf9feaaa8071e7af600c23e91ad
Nexxii <nexdev22@gmail.com>
2026-08-31 00:15:31 +0200

    bump protocol version

--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,5 @@
-pub const PROTOCOL = 2168;
-pub const MINECRAFT_VERSION = "1.26.40";
+pub const PROTOCOL = 2169;
+pub const MINECRAFT_VERSION = "1.26.44";
 pub const SHIELD_NETWORK_ID = 387;
 
 pub const NBT = @import("nbt");
